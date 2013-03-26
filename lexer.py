f56bb159e53e4519871fac17d5969584bdaba421
the-frey <acelynham@hotmail.com>
2013-03-26 20:29:29 +0000

    Basic functionality complete.

--- lexer.py
+++ lexer.py
@@ -10,7 +10,26 @@ tokens = (
 	'WORD', # Welcome!
 	)
 
+states = (
+	('htmlcomment','exclusive'),
+)
+
 t_ignore = ' ' #shortcut for whitespace
+
+def t_htmlcomment(token):
+	r'<!-{2}'
+	token.lexer.begin('htmlcomment') #begin this state
+
+def t_htmlcomment_end(token):
+	r'-{2}>'
+	token.lexer.lineno += token.value.count('\n') #add to whatever the current line number is
+	token.lexer.begin('INITIAL') #return to initial (normal) operation
+
+def t_htmlcomment_error(token):
+	token.lexer.skip(1) #while in html comment, skip all tokens not defined that will cause an error
+
+#if we wanted to display the comment or collect it, we could use something like r'<!-{2}.+-{2}>' 
+#then trim it with token.value = token.value[3:-3]
 
 #give us an indication of line number
 def t_newline(token):
@@ -45,6 +64,7 @@ def t_WORD(token):
 	return token
 
 webpage = """"This" is 
+<!-- my comment -->
 <b>my</b> webpage!
 """
 
